f1f94f035f317c9d452f60ee592e94340310971f
Christopher Fenton <cjf_uk@hotmail.com>
2020-05-15 22:42:11 +0100

    New lockdown iterator that had adjustable dates and two states. NOTE: Additional *1.csv and *5.csv are due to an outstanding error in MetaWards and will be deleted soon

--- ensembles/metawards/run_win.bat
+++ ensembles/metawards/run_win.bat
@@ -1,7 +1,13 @@
 @echo off
-if not exist "output" md "output"
-if not exist "plots" md "plots"
-python ..\..\tools\make_design\uq3a.py design.csv .\defs\limits.csv uq3a_out.csv -f
-python ..\..\tools\make_design\uq3b.py uq3a_out.csv disease.csv -f
-metawards -d ncov -o output --force-overwrite-output --input disease.csv -a ExtraSeedsLondon.dat --repeats 5 --extractor ..\..\tools\output_extractors\only_i_per_ward -u defs\lockdown_states.txt --iterator ..\..\tools\lockdown\iterate
-metawards-plot -i output\results.csv.bz2 -o plots
+if not exist "output1" md "output1"
+if not exist "plots1" md "plots1"
+if not exist "output5" md "output5"
+if not exist "plots5" md "plots5"
+python ..\..\tools\make_design\uq3a.py design1.csv .\defs\limits.csv uq3a_out1.csv -f
+python ..\..\tools\make_design\uq3a.py design5.csv .\defs\limits.csv uq3a_out5.csv -f
+python ..\..\tools\make_design\uq3b.py uq3a_out1.csv disease1.csv -f
+python ..\..\tools\make_design\uq3b.py uq3a_out5.csv disease5.csv -f
+metawards -d ncov -o output5 --force-overwrite-output --input disease5.csv -a ExtraSeedsLondon.dat --repeats 5 --extractor ..\..\tools\output_extractors\only_i_per_ward -u defs\lockdown_states.txt --iterator ..\..\tools\lockdown\iterate --start-date 2020/01/01
+metawards -d ncov -o output1 --force-overwrite-output --input disease1.csv -a ExtraSeedsLondon.dat --repeats 5 --extractor ..\..\tools\output_extractors\only_i_per_ward -u defs\lockdown_states.txt --iterator ..\..\tools\lockdown\iterate
+metawards-plot -i output5\results.csv.bz2 -o plots5
+metawards-plot -i output1\results.csv.bz2 -o plots1
