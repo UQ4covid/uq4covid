60963e95a03f7e6985323c14e59644ee259fa5a9
Christopher Fenton <cjf_uk@hotmail.com>
2020-05-20 19:24:54 +0100

    Ugly hack to uq4 command line tool to get a quick dataframe saved

--- tools/metawards_manager/uq4metawards/workflow/uq4.py
+++ tools/metawards_manager/uq4metawards/workflow/uq4.py
@@ -6,8 +6,10 @@ import argparse
 import sys
 import os
 import metawards
-import numpy as np
 from uq4metawards.utils import load_csv
+from uq4metawards.utils import print_progress_bar
+import pandas as pd
+import bz2 as compression
 
 
 def main():
@@ -17,46 +19,114 @@ def main():
     # Step 1 is to make an index file for the runs
     in_location = argv.disease
     data_location = argv.data
+    lookup_location = argv.lookup
     out_location = argv.output
     mode_str = "x"
     if argv.force:
         mode_str = "w"
         print("force option passed, disease matrix will be over-written if it exists")
 
+    #
+    # NOTE: Lockdown 1 = day 80, Lockdown 2 = day 133
+    #
+
     if argv.day < 0:
         print("Can't extract a negative day")
         sys.exit(1)
 
+    str_prefix = f"Building day tables for day {argv.day}"
+    str_suffix = f"completed"
+
     # Touch the file system
     try:
         with open(in_location) as disease_file, \
+                open(lookup_location) as ward_lookup_file, \
                 open(out_location, mode_str) as index_file:
 
             disease_file, disease_header_names = load_csv(disease_file)
+            ward_data = pd.read_csv(ward_lookup_file, quotechar='"')
             sub_dir_list = next(os.walk(data_location))[1]
 
-            num_experiments = 0
-            index = []
+            # Build output frame column names
+            n_wards = ward_data.shape[0]
+            col_names = disease_header_names[0:len(disease_header_names) - 1]
+            ward_cols = [f"ward[{x + 1}]" for x in range(n_wards)]
+            col_names += ward_cols
+
+            lads = ward_data.groupby("LAD11NM")
+            #v = lads.groups
+            #ids = ward_data["FID"].values
+            #lads = ward_data["LAD11NM"].values
+
+            # Calculate number of experiments from the disease file
+            n_experiments = int(sum(disease_file[:, disease_file.shape[1] - 1]))
+
+            # Day, Date, Design vars (no repeats), Wards
+            #n_cols = 2 + (disease_file.shape[1] - 1) + n_wards
+
+            # Fiddling with lists is cheaper than messing with dataframes
+            temp_list = [None] * n_experiments
+
+            experiment_index: int = 0
+            print("Loading data from MetaWards...")
+            print_progress_bar(experiment_index, n_experiments, str_prefix, str_suffix)
+
             for i, row in enumerate(disease_file):
-                # To have having to input the design file pick up repeats from regex
-                design_vars = row
-                string = metawards.VariableSet.create_fingerprint(design_vars)
 
-                # Find how many folders match this partial name
-                # TODO: Dir checks for data location
-                matches = [s for s in sub_dir_list if string in s]
-                run_id = 0
-                for m in matches:
-                    index.append((str("run_index_" + str(num_experiments)), m, str(i), str(run_id)))
+                # Calculate how many runs match this design
+                design_vars = row[0:len(row) - 1]
+                repeats = int(row[len(row) - 1])
+                if repeats < 1:
+                    raise ValueError("Invalid number of repeats, design file might be corrupted")
 
-                    disease_str = ','.join(str(x) for x in design_vars)
+                # Try to find each output folder
+                for r in range(repeats):
+                    string = metawards.VariableSet.create_fingerprint(design_vars, r + 1, True)
+                    if string not in sub_dir_list:
+                        raise ValueError("Missing run data for: " + string)
 
-                    num_experiments += 1
-                    run_id += 1
+                    wards_folder = os.path.join(data_location, string)
+                    wards_file = os.path.join(wards_folder, "wards_trajectory_I.csv.bz2")
 
-            index_header = ','.join(["key", "folder_id", "design_id", "run_id"])
-            index_matrix = np.asarray(index)
-            np.savetxt(fname=index_file, X=index_matrix, fmt="%s", delimiter=",", header=index_header, comments='')
+                    # Load the ward data (drop ward[0] as it is a placeholder)
+                    # We need to use pandas as there is more than one datatype in the output
+                    # TODO: ALL Of this is horrible, massive time pressure - FIXME all over
+                    # Check the console trace - really bad way of doing it, but whatever, time.
+                    con_file = os.path.join(wards_folder, "output.txt.bz2")
+                    with compression.open(con_file) as c_f:
+                        if b"scale_rate" not in c_f.read():
+                            print(f"Run {wards_file} is not valid")
+                            temp_list[experiment_index] = [0] * 8595
+                            flag = True
+                        else:
+                            flag = False
+
+                    if not flag:
+                        mw_out = pd.read_csv(wards_file).drop(["ward[0]", "date", "day"], 1)
+
+                        # NOTE: ANSI escape characters don't work in Windows Python
+                        # TODO: We shouldn't be using consoles on windows, use a (better) window instead.
+                        print(f"\rLoaded: {wards_file}")
+
+                        # TODO: mcar?
+                        try:
+                            data_row = mw_out.iloc[[argv.day]].values.squeeze().tolist()
+                            temp_list[experiment_index] = data_row
+                        except IndexError:
+                            print(f"Run: {wards_file} has no day {argv.day} - it will be substituted with zeros")
+                            temp_list[experiment_index] = [0] * 8595
+
+                    experiment_index += 1
+                    print_progress_bar(experiment_index, n_experiments, str_prefix, str_suffix)
+
+            print("Building output frames...")
+            w_out_by_day_full = pd.DataFrame(data=temp_list, columns=col_names)
+            w_out_by_day_full.to_csv(os.path.join(data_location, f"wards_by_day_{argv.day}.csv"))
+            print_progress_bar(experiment_index, n_experiments, str_prefix, str_suffix)
+
+            #index_header = ','.join(["key", "folder_id", "design_id", "run_id"])
+            #index_matrix = np.asarray(index)
+            #np.savetxt(fname=index_file, X=index_matrix, fmt="%s", delimiter=",", header=index_header, comments='')
 
     except FileExistsError:
         print("Output already exists, use -f to force overwriting")
@@ -79,6 +149,7 @@ def main_parser(main_args=None):
     parser = argparse.ArgumentParser("UQ4")
     parser.add_argument('disease', metavar='<input file>', type=str, help="Input disease matrix")
     parser.add_argument('data', metavar='<data folder>', type=str, help="MetaWards output folder")
+    parser.add_argument('lookup', metavar='<ward lookup file>', type=str, help="Ward data lookup file")
     parser.add_argument('output', metavar='<output file>', type=str, help="Output index file")
     parser.add_argument('day', metavar='<sim date>', type=int, help="Day to extract")
     parser.add_argument('-f', '--force', action='store_true', help="Force over-write")
