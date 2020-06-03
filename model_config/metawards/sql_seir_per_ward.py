6872b91dee214b83cb92c3da91f258639f3ceb84
Christopher Fenton <cjf_uk@hotmail.com>
2020-06-03 23:37:57 +0100

    Delta writing support for SQL

--- model_config/metawards/sql_seir_per_ward.py
+++ model_config/metawards/sql_seir_per_ward.py
@@ -15,19 +15,18 @@
 #   output_table.name = "infected" and ward_id = "x"
 #
 
+# NOTE: The import list has been made as pure as possible on request, but other libraries might do things better
+import metawards
 from metawards.utils import call_function_on_network
 from metawards.utils import Console
-import metawards
-import multiprocessing
+from multiprocessing import Lock
 from typing import List, Union
-import sqlite3 as sql
-import sys
-import os
-import csv
+from sqlite3 import OperationalError, Connection, Cursor, connect
+from sys import argv
+from os import path
+from csv import reader
 
-_output_channels = {"infected": -1, "removed": -1}  # Output channels for the database
 _sql_file_name: str = ""
-_run_ident = ""
 _run_index: int = -1  # Which run this currently is
 
 # Previous outputs (for delta writes)
@@ -36,28 +35,28 @@ _prev_e: Union[List[int], None] = None
 _prev_i: Union[List[int], None] = None
 _prev_r: Union[List[int], None] = None
 
-# This is a bit dodgy, but skirts import/unresolved object errors for now
+# This is a bit dodgy as we use eval(), but skirts import/unresolved object errors for now
 # TODO: Write mini data_src functional objects that wrap the workspace to resolve this
 _output_channels_list = \
     {
-        "susceptible": ["workspace.S_in_wards", _prev_s],
-        "exposed": ["workspace.E_in_wards", _prev_e],
-        "infected": ["workspace.I_in_wards", _prev_i],
-        "removed": ["workspace.R_in_wards", _prev_r]
+        "susceptible": ["workspace.S_in_wards", "deltas_s"],
+        "exposed": ["workspace.E_in_wards", "deltas_e"],
+        "infected": ["workspace.I_in_wards", "deltas_i"],
+        "removed": ["workspace.R_in_wards", "deltas_r"]
     }
 
 
 # Horrible hack to get access to input file argument not available in the extractor
 # NOTE: Using metawards.app.run.parse_args() will raise an exception - no free lunch!
 def get_input_file_name() -> str:
-    return next((sys.argv[i + 1] for i, x in enumerate(sys.argv) if x == "-i" or x == "--input"), None)
+    return next((argv[i + 1] for i, x in enumerate(argv) if x == "-i" or x == "--input"), None)
 
 
 # Get the design data from the input file - this is probably a member function somewhere
 # This is needed because the user variables lose their dots and it is impossible to find the hypercube parameters
 def get_design_data() -> (List[str], List[List[str]]):
     with open(get_input_file_name()) as in_file:
-        data: List[List[str]] = list(csv.reader(in_file))
+        data: List[List[str]] = list(reader(in_file))
         header = data[0]
         data.pop(0)
         return header, data
@@ -66,22 +65,25 @@ def get_design_data() -> (List[str], List[List[str]]):
 # Makes the sql database
 # We can pass strings to metawards, so this can be removed at some point
 # TODO: Look into removing this (make template schema and copy) - environment variables might work?
-def make_sql_template(out_connection: sql.Connection):
+def make_sql_template(out_connection: Connection):
     header, data = get_design_data()
     design_names = [var[1:] for var in header[:-1] if var[0] == '.']
     column_indices = [i for i, s in enumerate(header[:-1]) for name in design_names if name in s]
-    database: Union[sql.Connection, None] = None
+    database: Union[Connection, None] = None
 
     # If the operations fail, we cannot continue, but we must close the connection (try without except)
     try:
 
-        # Table idents
+        # Table idents: 5 is enough to stitch the entire ward output together with the hypercube designs
+        # These should ensure third normal form, although I haven't checked this in detail
         design_table_name: str = "design_table"
         output_table_name: str = "output_table"
         day_table_name: str = "day_table"
         run_table_name: str = "run_table"
         results_table_name: str = "results_table"
 
+        # The design table is a dynamic schema constructed from all the "." variables in the input table
+        # We assume the first column is the key (TODO: Use a complex primary key over the row instead?)
         # First design column is an integer key, rest are hypercube points on [-1, 1]
         var_schema: List[str] = []
         check_schema: List[str] = []
@@ -105,8 +107,8 @@ def make_sql_template(out_connection: sql.Connection):
                                     f"foreign key (run_id) references {run_table_name}(run_index));"
 
         # Create in memory first (small database, + we want to ensure it is well-formed)
-        database = sql.connect(":memory:")
-        cursor: sql.Cursor = database.cursor()
+        database = connect(":memory:")
+        cursor: Cursor = database.cursor()
 
         # Flags / pragmas
         cursor.execute(f'PRAGMA encoding = "UTF-8";')
@@ -144,40 +146,39 @@ def make_sql_template(out_connection: sql.Connection):
 def extractor_setup(network: metawards.Network, **kwargs):
     # Globals
     global _sql_file_name
-    global _run_ident
 
     # Get the unique output directory for this run
     out_object: metawards.OutputFiles = kwargs["output_dir"]
-    run_ident = os.path.basename(out_object.get_path())
-    out_folder = os.path.abspath(os.path.join(out_object.get_path(), os.path.pardir))
+    run_ident = path.basename(out_object.get_path())
+    out_folder = path.abspath(path.join(out_object.get_path(), path.pardir))
 
     # Identify the design point
     design_index = int(network.params.user_params["design_index"])
 
     # Create a valid URI to use the SQLite access rights
-    data_base_file_name: str = os.path.join(out_folder, "sql_wards.dat")
+    data_base_file_name: str = path.join(out_folder, "sql_wards.dat")
     _sql_file_name = data_base_file_name
-    fixed_path = os.path.abspath(data_base_file_name).replace("\\", "/")
+    fixed_path = path.abspath(data_base_file_name).replace("\\", "/")
     db_uri = f"file:{fixed_path}?mode=rw"
 
     # Concurrency guard: multiple processes can connect, but only one should create
     # NOTE: Connections only fail if the file doesn't exist with mode "rw"
-    test_connection: Union[sql.Connection, None] = None
-    create_connection: Union[sql.Connection, None] = None
+    test_connection: Union[Connection, None] = None
+    create_connection: Union[Connection, None] = None
 
     # One at a time here
-    mutex = multiprocessing.Lock()
+    mutex = Lock()
     with mutex:
         try:
-            test_connection = sql.connect(db_uri, uri=True)
+            test_connection = connect(db_uri, uri=True)
             # TODO: What do we do if there is an old database in there?
             # FIXME: For re-running partial ensembles this needs to be handled
-        except sql.OperationalError:
+        except OperationalError:
             # This process is the first one in to create the database
             try:
                 # Append "c" to make the connection create a blank database
-                create_connection = sql.connect(db_uri + "c", uri=True)
-            except sql.OperationalError as err:
+                create_connection = connect(db_uri + "c", uri=True)
+            except OperationalError as err:
                 # An actual problem happened if we get here
                 Console.print("SQL Error: " + str(err))
                 raise
@@ -190,7 +191,7 @@ def extractor_setup(network: metawards.Network, **kwargs):
 
         # Do first commits for setting up the run
         # TODO: Preserve connection across scope
-        process_connection = sql.connect(_sql_file_name)
+        process_connection = connect(_sql_file_name)
         try:
             write_setup_entries(process_connection, design_index, run_ident)
         finally:
@@ -199,9 +200,9 @@ def extractor_setup(network: metawards.Network, **kwargs):
 
 
 # Setup entries which are written once per run
-def write_setup_entries(database: sql.Connection, design_index: int, run_ident: str):
+def write_setup_entries(database: Connection, design_index: int, run_ident: str):
     global _run_index
-    c: sql.Cursor = database.cursor()
+    c: Cursor = database.cursor()
 
     # Write this run
     i_str = f"insert into run_table(design_index,end_day,mw_folder) values (?,?,?)"
@@ -219,6 +220,8 @@ def output_wards_ir_serial(network: metawards.Network, population: metawards.Pop
                            workspace: metawards.Workspace, **kwargs):
     global _sql_file_name
     global _run_index
+    global _prev_s
+    global _prev_e
     global _prev_i
     global _prev_r
 
@@ -228,8 +231,8 @@ def output_wards_ir_serial(network: metawards.Network, population: metawards.Pop
         extractor_setup(network, **kwargs)
 
     # Prepare database entries
-    database = sql.connect(_sql_file_name)
-    c: sql.Cursor = database.cursor()
+    database = connect(_sql_file_name)
+    c: Cursor = database.cursor()
 
     # Write the current day, some may be longer / shorter, so ignore duplicate entries
     c.execute(f"insert or ignore into day_table(day,date) values (?,?)", (int(population.day), str(population.date)))
@@ -239,35 +242,41 @@ def output_wards_ir_serial(network: metawards.Network, population: metawards.Pop
     # NOTE: Don't re-use time_index as if there is a duplicate then the rowid will be zero
     # TODO: List comprehension is fine, but consider numpy (or equivalent) for more speed
 
+    output_src = 0
+    mode_str = "normal write"
     deltas = network.params.user_params["deltas"]
     if deltas:
         # Write deltas - This saves about ~6% total space and increases the compression factor by 150%
+        # NOTE: Assumption - if one is None, the rest of them are (this isn't good!!)
+
         if _prev_i is None:
-            _prev_i = workspace.I_in_wards
-            _prev_r = workspace.R_in_wards
+            deltas_s = workspace.S_in_wards
+            deltas_e = workspace.E_in_wards
             deltas_i = workspace.I_in_wards
             deltas_r = workspace.R_in_wards
         else:
+            deltas_s = [a - b for a, b in zip(workspace.S_in_wards, _prev_s)]
+            deltas_e = [a - b for a, b in zip(workspace.E_in_wards, _prev_e)]
             deltas_i = [a - b for a, b in zip(workspace.I_in_wards, _prev_i)]
             deltas_r = [a - b for a, b in zip(workspace.R_in_wards, _prev_r)]
-            _prev_i = workspace.I_in_wards
-            _prev_r = workspace.R_in_wards
+        _prev_s = workspace.S_in_wards
+        _prev_e = workspace.E_in_wards
+        _prev_i = workspace.I_in_wards
+        _prev_r = workspace.R_in_wards
 
-        v_infected = [(i, _output_channels["infected"], x, int(population.day), _run_index)
-                      for i, x in enumerate(deltas_i) if i != 0]
-        v_removed = [(i, _output_channels["removed"], x, int(population.day), _run_index)
-                     for i, x in enumerate(deltas_r) if i != 0]
-        c.executemany(f"insert into results_table(ward_id,output_channel,sim_out,sim_time,run_id) values (?,?,?,?,?)",
-                      v_infected + v_removed)
-    else:
-        # The index has already been sent to the database, so reuse it here safely
-        for channel_index, channel_name in enumerate(_output_channels_list.keys()):
-            Console.print(f"Writing channel {channel_name} to database")
-            data_source = eval(_output_channels_list[channel_name][0])
-            values = [(i, channel_index, x, int(population.day), _run_index)
-                      for i, x in enumerate(data_source) if i != 0]
-            c.executemany(f"insert into results_table(ward_id,output_channel,sim_out,sim_time,run_id) "
-                          f"values (?,?,?,?,?)", values)
+        # Point eval() at deltas
+        output_src = 1
+        mode_str = "delta write"
+
+    # The index has already been sent to the database, so reuse it here safely
+    for channel_index, channel_name in enumerate(_output_channels_list.keys()):
+        Console.print(f"Writing channel {channel_name} to database src = "
+                      f"{_output_channels_list[channel_name][output_src]}: {mode_str}")
+        data_source = eval(_output_channels_list[channel_name][output_src])
+        values = [(i, channel_index, x, int(population.day), _run_index)
+                  for i, x in enumerate(data_source) if i != 0]
+        c.executemany(f"insert into results_table(ward_id,output_channel,sim_out,sim_time,run_id) "
+                      f"values (?,?,?,?,?)", values)
 
     # Write last day into run table
     c.execute("update run_table set end_day = ? where run_index = ?", (int(population.day), _run_index))
@@ -285,6 +294,6 @@ def output_wards_i(nthreads: int = 1, **kwargs):
 
 # NOTE: We test for setup by sticking a flag to the network
 # TODO: Find a better solution
-def extract(network: metawards.Network, **kwargs) -> List[metawards.utils.MetaFunction]:
+def extract(**kwargs) -> List[metawards.utils.MetaFunction]:
     Console.print(f"Sending I and R per ward to the output stream")
     return [output_wards_i]
