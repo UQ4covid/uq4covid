e985aba8e11c7d64dd9b87f86b624e017342978c
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-06-05 16:47:57 +0100

    Added bash install script.

---
+++ tools/metawards_manager/srcinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This will rebuild and install a local copy of metawards_manager
+
+python -m pip install --upgrade pip
+python -m pip install --user --upgrade setuptools wheel
+python setup.py sdist bdist_wheel
+pip install .
